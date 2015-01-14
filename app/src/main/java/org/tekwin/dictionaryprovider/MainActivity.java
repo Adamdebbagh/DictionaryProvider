a225fd8e385ca7fde236a7f739063fbfb7e4da7c
Adam Debbagh <adamdebbagh@gmail.com>
2015-01-14 17:59:41 -0500

    query the source data using the dictionary content provider and diplay it in the main ui

--- app/src/main/java/org/tekwin/dictionaryprovider/MainActivity.java
+++ app/src/main/java/org/tekwin/dictionaryprovider/MainActivity.java
@@ -5,9 +5,7 @@ import android.database.Cursor;
 import android.os.Bundle;
 import android.provider.UserDictionary;
 import android.support.v7.app.ActionBarActivity;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.ListView;
+import android.widget.TextView;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -18,39 +16,47 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
 
         // Get the TextView which will be populated with the Dictionary ContentProvider data.
-        ListView dictListView = (ListView) findViewById(R.id.dictionary_list_view);
+        TextView dictTextView = (TextView) findViewById(R.id.dictionary_text_view);
 
 
-        // Get the ContentResolver which will send a message to the ContentProvider.
+        // Get the ContentResolver which will send a message to the ContentProvider
         ContentResolver resolver = getContentResolver();
 
 
-        // Get a Cursor containing all of the rows in the Words table.
+        // Get a Cursor containing all of the rows in the Words table
         Cursor cursor = resolver.query(UserDictionary.Words.CONTENT_URI, null, null, null, null);
 
+        // Surround the cursor in a try statement so that the finally block will eventually execute
+        try {
+            dictTextView.setText("The UserDictionary contains " + cursor.getCount() + " words\n");
+            dictTextView.append("COLUMNS: " + UserDictionary.Words._ID  + " - " + UserDictionary.Words.FREQUENCY +
+                    " - " + UserDictionary.Words.WORD);
 
-    }
+
+            // Get the index of the columns using UserDictionary.Words constants,
+            // which contain the headers of the columns.
+            int idColumn = cursor.getColumnIndex(UserDictionary.Words._ID);
+            int frequencyColumn = cursor.getColumnIndex(UserDictionary.Words.FREQUENCY);
+            int wordColumn = cursor.getColumnIndex(UserDictionary.Words.WORD);
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+            // Iterates through all returned rows in the cursor.
+            while (cursor.moveToNext()) {
+                // Use that index to extract the String value of the word
+                // at the current row the cursor is on.
+                int id = cursor.getInt(idColumn);
+                int frequency = cursor.getInt(frequencyColumn);
+                String word = cursor.getString(wordColumn);
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+                dictTextView.append(("\n" + id + " - " + frequency + " - " + word));
+            }
+        } finally {
+            // Always close your cursor to avoid memory leaks
+            cursor.close();
         }
-
-        return super.onOptionsItemSelected(item);
     }
+
+
+
 }
